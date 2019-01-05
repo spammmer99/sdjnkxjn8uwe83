af6e229f97a2fb210afb8238f855b95d38a00d58
spammmer99 <46374080+spammmer99@users.noreply.github.com>
2019-01-05 08:18:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -86,5 +86,3 @@ if (message.content === '!spam') {
 
 client.login(process.env.TOKEN);// لا تغير فيها شيء
 client2.login(process.env.TOKEN2);// لا تغير فيها شيء
-client2.login(process.env.TOKEN3);
-client2.login(process.env.TOKEN4);
